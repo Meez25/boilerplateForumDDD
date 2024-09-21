83eec69e138db8a53cfd2c936ea8e22cfdfecafe
Yann BRONNER <yann.bronner@gmail.com>
2024-09-21 15:55:52 +0200

    good stuff

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,16 @@ package main
 import (
 	"fmt"
 
-	// "github.com/meez25/boilerplateForumDDD/category/infrastructure"
-	// "github.com/meez25/boilerplateForumDDD/internal/category"
-	"github.com/meez25/boilerplateForumDDD/internal/user"
-	"github.com/meez25/boilerplateForumDDD/user/infrastructure"
+	"github.com/meez25/boilerplateForumDDD/application/services"
+	"github.com/meez25/boilerplateForumDDD/infrastructure/persistence"
 )
 
 func main() {
-	// This is the main function
-
-	userRepo := infrastructure.NewUserMemoryRepository()
-	userService := user.NewUserService(userRepo)
+	userRepo := persistence.NewUserMemoryRepository()
+	userService := services.NewUserService(userRepo)
 
 	// Create a new user
-	newUser, err := user.NewUser("meez25", "yann@memofamille.com", "password", "Yann", "Meez")
+	newUser, err := userService.Create("John Doe", "johnDoe@doe.com", "password", "john", "doe")
 
 	// Save the user
 	err = userService.Register(newUser)
@@ -39,25 +35,25 @@ func main() {
 	// ----------------------------
 	// Create a category
 
-	// categoryRepo := infrastructure.NewCategoryMemoryRepo()
-	// categoryService := category.NewCategoryService(categoryRepo)
-	//
-	// cat, err := categoryService.Create("Category 1", "Description 1", nil)
-	//
-	// if err != nil {
-	// 	panic(err)
-	// }
-	//
-	// fmt.Println("Category created successfully")
-	//
-	// // Find the category by ID
-	//
-	// foundCat, err := categoryService.FindByID(cat.ID.String())
-	//
-	// if err != nil {
-	// 	panic(err)
-	// }
-	//
-	// fmt.Println("Category found by ID:", foundCat)
+	categoryRepo := persistence.NewCategoryMemoryRepo()
+	categoryService := services.NewCategoryService(categoryRepo)
+
+	cat, err := categoryService.Create("Category 1", "Description 1", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Category created successfully")
+
+	// Find the category by ID
+
+	foundCat, err := categoryService.FindByID(cat.ID.String())
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Category found by ID:", foundCat)
 
 }
